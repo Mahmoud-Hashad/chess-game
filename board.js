eb506681827c3126050cffdf353666f4d3b9b2a6
Mahmoud-Hashad <mahmoud.explour@gmail.com>
2020-02-25 14:04:47 +0200

    fix board color

--- board.js
+++ board.js
@@ -12,7 +12,7 @@ class Board {
     for (let i = 0; i < this.squaresByLine; i++) {
       for (let j = 0; j < this.squaresByLine; j++) {
         noStroke();
-        if (i % 2 == 0) color ? fill("#CC6633") : fill("#ffb070");
+        if (i % 2) color ? fill("#CC6633") : fill("#ffb070");
         else color ? fill("#ffb070") : fill("#CC6633");
         color = !color;
         rect(j * squareWidth, i * squareheight, squareWidth, squareheight);
